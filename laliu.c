ff9051346c40c0d666d8f64d9ac6fdf140934975
meisa233 <39454391+meisa233@users.noreply.github.com>
2021-11-03 15:50:37 +0800

    增加退出机制
    先按q再按回车

--- laliu.c
+++ laliu.c
@@ -1,8 +1,94 @@
+#include "laliu.h"
 #include <stddef.h>
+#include <stdlib.h>
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 
+#if HAVE_SYS_SELECT_H
+#include <sys/select.h>
+#endif
+
+#if HAVE_TERMIOS_H
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <termios.h>
+#elif HAVE_KBHIT
+#include <conio.h>
+#endif
+
 static int64_t s_time = 0;
+static int64_t last_time = -1;
+
+static int read_key(void)
+{
+    unsigned char ch;
+#if HAVE_TERMIOS_H
+    int n = 1;
+    struct timeval tv;
+    fd_set rfds;
+
+    FD_ZERO(&rfds);
+    FD_SET(0, &rfds);
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    n = select(1, &rfds, NULL, NULL, &tv);
+    if (n > 0) {
+        n = read(0, &ch, 1);
+        if (n == 1)
+            return ch;
+
+        return n;
+    }
+#elif HAVE_KBHIT
+#    if HAVE_PEEKNAMEDPIPE
+    static int is_pipe;
+    static HANDLE input_handle;
+    DWORD dw, nchars;
+    if(!input_handle){
+        input_handle = GetStdHandle(STD_INPUT_HANDLE);
+        is_pipe = !GetConsoleMode(input_handle, &dw);
+    }
+
+    if (is_pipe) {
+        /* When running under a GUI, you will end here. */
+        if (!PeekNamedPipe(input_handle, NULL, 0, NULL, &nchars, NULL)) {
+            // input pipe may have been closed by the program that ran ffmpeg
+            return -1;
+        }
+        //Read it
+        if(nchars != 0) {
+            read(0, &ch, 1);
+            return ch;
+        }else{
+            return -1;
+        }
+    }
+#    endif
+    if(kbhit())
+        return(getch());
+#endif
+    return -1;
+}
+
+static int check_keyboard_interaction(int64_t cur_time){
+    static int64_t last_time;
+    int key;
+    //printf("cur_time - last_time = %ld\n", cur_time - last_time);
+    if(cur_time - last_time >= 100000){
+        key = read_key();
+        //printf("cur_time - last_time >= 100000\n");
+        //printf("%c\n", key);
+        last_time = cur_time;
+    }else
+    {key = -1;}
+    
+    if (key == 'q'){
+        //printf("bingo!");
+        return -1;
+    }
+    return 0;
+}
 
 void set_block_timeout_time(int64_t time)
 {
@@ -68,7 +154,7 @@ int main(int argc, char * argv[])
     int i;
     int got_picture;
 
-
+    static int64_t last_time;
     if(argc < 2){
         printf("Usage: a.out <in_file> <out_file>");
         return -1;
@@ -83,7 +169,7 @@ int main(int argc, char * argv[])
     pInFmtContext = avformat_alloc_context();
     pInFmtContext->interrupt_callback.callback = interrupt_cb;
     pInFmtContext->interrupt_callback.opaque = pInFmtContext;
-    set_block_timeout_time(get_systime() + 10*1000000);
+    set_block_timeout_time(get_systime() + 5 * 1000000);
     // Open an input stream and read the header, 
     if( (ret = avformat_open_input ( &pInFmtContext, in_file, NULL, NULL)) < 0){
         printf("avformat_open_input failed:%d",ret);         
@@ -96,7 +182,7 @@ int main(int argc, char * argv[])
 	}
     //print 
 	av_dump_format(pInFmtContext, 0, in_file, 0); 
-
+	
 // output ..............................
     ret = avformat_alloc_output_context2(&pOutFmtContext, NULL, NULL, out_file);
     if(ret < 0){
@@ -144,9 +230,14 @@ int main(int argc, char * argv[])
     }
 
     in_packet = av_packet_alloc();
+    last_time = get_systime();
 
     while(1){
-        set_block_timeout_time(get_systime() + 8000000);
+        int64_t cur_time = get_systime();
+        //printf("cur_time is %ld\n", cur_time);
+        if (check_keyboard_interaction(cur_time) < 0)
+            break;
+        set_block_timeout_time(get_systime() + 2000000);
         ret = av_read_frame(pInFmtContext, in_packet);
         if(ret < 0)
             break;
@@ -158,24 +249,25 @@ int main(int argc, char * argv[])
 
         if(in_packet->stream_index == videoindex){
             video_frame_size += in_packet->size;
-            printf("recv %5d video frame %5d-%5d", ++video_frame_count, in_packet->size, video_frame_size);
+            printf("recv %5d video frame %5d-%5d\n", ++video_frame_count, in_packet->size, video_frame_size);
         }
 
         
         if(in_packet->stream_index == audioindex){
             audio_frame_size += in_packet->size;
-            printf("recv %5d audio frame %5d-%5d", ++audio_frame_count, in_packet->size, audio_frame_size);
+            printf("recv %5d audio frame %5d-%5d\n", ++audio_frame_count, in_packet->size, audio_frame_size);
         }
 
         ret = av_interleaved_write_frame(pOutFmtContext, in_packet);
         if(ret < 0){
-            printf("av_interleaved_write_frame failed");
+            printf("av_interleaved_write_frame failed\n");
             break;
         }
         av_packet_unref(in_packet);
 
     }
 
+end:
     av_write_trailer(pOutFmtContext);
     av_packet_free(&in_packet);
 
@@ -183,6 +275,8 @@ int main(int argc, char * argv[])
     avio_close( pOutFmtContext->pb);
     avformat_free_context(pOutFmtContext);
 
-    printf("................end");
+    printf("................end\n");
+   
     return 0;
 }
+
